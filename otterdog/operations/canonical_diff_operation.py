29cb6205ffb2db2ec96a2d7c6e1087ca76efbe7a
Thomas Neidhart <thomas.neidhart@eclipse-foundation.org>
2023-05-26 16:34:49 +0200

    Do not indent diff output so that comments in github PRs can be rendered with color.

--- otterdog/operations/canonical_diff_operation.py
+++ otterdog/operations/canonical_diff_operation.py
@@ -29,49 +29,44 @@ class CanonicalDiffOperation(Operation):
         github_id = org_config.github_id
 
         self.printer.print(f"Organization {Style.BRIGHT}{org_config.name}{Style.RESET_ALL}[id={github_id}]")
-        self.printer.level_up()
+
+        org_file_name = self.jsonnet_config.get_org_config_file(github_id)
+
+        if not os.path.exists(org_file_name):
+            self.printer.print_warn(f"configuration file '{org_file_name}' does not yet exist, run fetch first")
+            return 1
 
         try:
-            org_file_name = self.jsonnet_config.get_org_config_file(github_id)
+            organization = \
+                GitHubOrganization.load_from_file(github_id,
+                                                  self.jsonnet_config.get_org_config_file(github_id),
+                                                  self.config,
+                                                  False)
+        except RuntimeError as ex:
+            self.printer.print_warn(f"failed to load configuration: {str(ex)}")
+            return 1
 
-            if not os.path.exists(org_file_name):
-                self.printer.print_warn(f"configuration file '{org_file_name}' does not yet exist, run fetch first")
-                return 1
+        with open(org_file_name, "r") as file:
+            original_config = file.read()
 
-            try:
-                organization = \
-                    GitHubOrganization.load_from_file(github_id,
-                                                      self.jsonnet_config.get_org_config_file(github_id),
-                                                      self.config,
-                                                      False)
-            except RuntimeError as ex:
-                self.printer.print_warn(f"failed to load configuration: {str(ex)}")
-                return 1
+        original_config_without_comments: list[str] = \
+            list(filter(lambda x: not x.strip().startswith("#"), original_config.split("\n")))
 
-            with open(org_file_name, "r") as file:
-                original_config = file.read()
+        canonical_config = organization.to_jsonnet(self.jsonnet_config)
+        canonical_config_as_lines = canonical_config.split("\n")
 
-            original_config_without_comments: list[str] = \
-                list(filter(lambda x: not x.strip().startswith("#"), original_config.split("\n")))
+        if original_config_without_comments[-1] == "":
+            original_config_without_comments.pop(-1)
 
-            canonical_config = organization.to_jsonnet(self.jsonnet_config)
-            canonical_config_as_lines = canonical_config.split("\n")
+        for line in difflib.unified_diff(original_config_without_comments,
+                                         canonical_config_as_lines,
+                                         "original",
+                                         "canonical"):
+            if line.startswith("+"):
+                self.printer.print(f"{Fore.GREEN}{line}{Style.RESET_ALL}")
+            elif line.startswith("-"):
+                self.printer.print(f"{Fore.RED}{line}{Style.RESET_ALL}")
+            else:
+                self.printer.print(line)
 
-            if original_config_without_comments[-1] == "":
-                original_config_without_comments.pop(-1)
-
-            for line in difflib.unified_diff(original_config_without_comments,
-                                             canonical_config_as_lines,
-                                             "original",
-                                             "canonical"):
-                if line.startswith("+"):
-                    self.printer.print(f"{Fore.GREEN}{line}{Style.RESET_ALL}")
-                elif line.startswith("-"):
-                    self.printer.print(f"{Fore.RED}{line}{Style.RESET_ALL}")
-                else:
-                    self.printer.print(line)
-
-            return 0
-
-        finally:
-            self.printer.level_down()
+        return 0
