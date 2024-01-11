f4c348e6111c4d2f1294040cfe6b80127cdf00d2
Thomas Neidhart <thomas.neidhart@eclipse-foundation.org>
2024-01-11 19:08:42 +0100

    Change validation warning to info for workflow settings that are coerced from org level settings

--- otterdog/models/repo_workflow_settings.py
+++ otterdog/models/repo_workflow_settings.py
@@ -108,7 +108,7 @@ class RepositoryWorkflowSettings(WorkflowSettings):
                 and self.default_workflow_permissions == "write"
             ):
                 context.add_failure(
-                    FailureType.WARNING,
+                    FailureType.INFO,
                     f"{self.get_model_header(parent_object)} has 'default_workflow_permissions' of value "
                     f"'{self.default_workflow_permissions}', "
                     f"while on organization level it is restricted to "
@@ -120,7 +120,7 @@ class RepositoryWorkflowSettings(WorkflowSettings):
                 and self.actions_can_approve_pull_request_reviews is True
             ):
                 context.add_failure(
-                    FailureType.WARNING,
+                    FailureType.INFO,
                     f"{self.get_model_header(parent_object)} has 'actions_can_approve_pull_request_reviews' "
                     "enabled, while on organization level it is disabled, setting will be ignored.",
                 )
