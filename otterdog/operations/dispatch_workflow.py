dc96eed69b2caa4641b985bb31b8b49bd574f3b5
Thomas Neidhart <thomas.neidhart@eclipse-foundation.org>
2024-01-23 11:36:43 +0100

    Change license to EPL v2

--- otterdog/operations/dispatch_workflow.py
+++ otterdog/operations/dispatch_workflow.py
@@ -1,9 +1,9 @@
 #  *******************************************************************************
 #  Copyright (c) 2023-2024 Eclipse Foundation and others.
 #  This program and the accompanying materials are made available
-#  under the terms of the MIT License
-#  which is available at https://spdx.org/licenses/MIT.html
-#  SPDX-License-Identifier: MIT
+#  under the terms of the Eclipse Public License 2.0
+#  which is available at http://www.eclipse.org/legal/epl-v20.html
+#  SPDX-License-Identifier: EPL-2.0
 #  *******************************************************************************
 
 from otterdog.config import OrganizationConfig
--- tests/models/__init__.py
+++ tests/models/__init__.py
@@ -1,10 +1,10 @@
-# *******************************************************************************
-# Copyright (c) 2023 Eclipse Foundation and others.
-# This program and the accompanying materials are made available
-# under the terms of the MIT License
-# which is available at https://spdx.org/licenses/MIT.html
-# SPDX-License-Identifier: MIT
-# *******************************************************************************
+#  *******************************************************************************
+#  Copyright (c) 2023-2024 Eclipse Foundation and others.
+#  This program and the accompanying materials are made available
+#  under the terms of the Eclipse Public License 2.0
+#  which is available at http://www.eclipse.org/legal/epl-v20.html
+#  SPDX-License-Identifier: EPL-2.0
+#  *******************************************************************************
 
 import json
 import os
