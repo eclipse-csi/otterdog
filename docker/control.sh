b65e7988d662ee990fb6087e003f475eaa310e77
Thomas Neidhart <thomas.neidhart@eclipse-foundation.org>
2024-01-26 12:02:41 +0100

    churn: update docker setup

---
+++ docker/control.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+#
+#  *******************************************************************************
+#  Copyright (c) 2024 Eclipse Foundation and others.
+#  This program and the accompanying materials are made available
+#  under the terms of the Eclipse Public License 2.0
+#  which is available at http://www.eclipse.org/legal/epl-v20.html
+#  SPDX-License-Identifier: EPL-2.0
+#  *******************************************************************************
+#
+
+set -e
+
+project=otterdog
+
+printHelp () {
+	echo "Usage: control.sh <command>"
+	echo "Available commands:"
+	echo
+	echo "   start        Pulls changes, builds docker image(s), and starts"
+	echo "                the services (Nginx, Node.js)."
+	echo "   startdev     Pulls changes, builds docker image(s), and starts"
+	echo "                the services (Nginx, Node.js)."
+	echo
+	echo "   reloadnginx  Reloads the nginx configuration"
+	echo
+	echo "   stop         Stops the services."
+	echo
+	echo "   logs         Tail -f services' logs."
+	echo
+	echo "   shell        Opens a shell into the webapp container."
+	echo
+	echo "   shellnginx   Opens a shell into the Nginx container."
+}
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+pushd $dir > /dev/null
+
+case "$1" in
+start)
+	docker compose -p $project -f docker-compose.base.yml -f docker-compose.prod.yml build
+	docker compose -p $project -f docker-compose.base.yml -f docker-compose.prod.yml up -d
+	;;
+startdev)
+	docker-compose -p $project -f docker-compose.base.yml down -t 1
+	docker-compose -p $project -f docker-compose.base.yml -f docker-compose.dev.yml build
+	docker-compose -p $project -f docker-compose.base.yml -f docker-compose.dev.yml up
+	;;
+reloadnginx)
+	docker exec -it ${project}_nginx nginx -t
+	docker exec -it ${project}_nginx nginx -s reload
+	;;
+stop)
+	docker-compose -p $project -f docker-compose.base.yml down -t 1
+	;;
+shell)
+	docker exec -it ${project}_server bash
+	;;
+shellnginx)
+	docker exec -it ${project}_nginx bash
+	;;
+logs)
+	docker-compose -p $project -f docker-compose.base.yml logs -f
+	;;
+*)
+	echo "Invalid command $1"
+	printHelp
+	;;
+esac
+
+popd > /dev/null
