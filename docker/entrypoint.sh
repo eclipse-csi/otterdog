e90fbdc9f1b32748234f0c3604d7279ba2ee49f9
Thomas Neidhart <thomas.neidhart@eclipse-foundation.org>
2024-03-22 08:20:00 +0100

    churn: fix license header

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#
+#  *******************************************************************************
+#  Copyright (c) 2024 Eclipse Foundation and others.
+#  This program and the accompanying materials are made available
+#  under the terms of the Eclipse Public License 2.0
+#  which is available at http://www.eclipse.org/legal/epl-v20.html
+#  SPDX-License-Identifier: EPL-2.0
+#  *******************************************************************************
+#
+
 # if any of the commands in your code fails for any reason, the entire script fails
 set -o errexit
 # fail exit if one of your pipe command fails
