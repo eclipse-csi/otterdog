c3ffde61fdb6d11716538080745138fa2d8164df
Mikaël Barbero <mikael.barbero@eclipse-foundation.org>
2025-06-16 06:17:42 -0400

    Update otterdog/operations/check_status.py
    Co-authored-by: Kairo Araujo <kairo@dearaujo.nl>
    Signed-off-by: Mikaël Barbero <mikael.barbero@gmail.com>

--- otterdog/operations/check_status.py
+++ otterdog/operations/check_status.py
@@ -1,5 +1,5 @@
 #  *******************************************************************************
-#  Copyright (c) 2023-2024 Eclipse Foundation and others.
+#  Copyright (c) 2023-2025 Eclipse Foundation and others.
 #  This program and the accompanying materials are made available
 #  under the terms of the Eclipse Public License 2.0
 #  which is available at http://www.eclipse.org/legal/epl-v20.html
