6eed20fbd781fceeb2cda85a9e048863df7de5ff
Kairo Araujo <kairo@dearaujo.nl>
2025-11-21 10:17:29 +0100

    refactor: remove duplicated multiple list (#528)
    * refactor: remove duplicated multiple list
    
    closes #527
    
    Signed-off-by: Kairo Araujo <kairo.araujo@eclipse-foundation.org>
    
    * add assert for csv_fields (noqa S101)
    
    Signed-off-by: Kairo Araujo <kairo.araujo@eclipse-foundation.org>
    
    * change typing
    
    Signed-off-by: Kairo Araujo <kairo.araujo@eclipse-foundation.org>
    
    * tests: add unit tests
    
    - create MockGitHubProvider fixture to enable easy mock
    
    Signed-off-by: Kairo Araujo <kairo.araujo@eclipse-foundation.org>
    
    * simplify assert and parameters
    
    Signed-off-by: Kairo Araujo <kairo.araujo@eclipse-foundation.org>
    
    ---------
    
    Signed-off-by: Kairo Araujo <kairo.araujo@eclipse-foundation.org>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from collections.abc import Mapping
 from typing import Any
 
+import pretend
 import pytest
 
 from otterdog.jsonnet import JsonnetConfig
@@ -28,3 +29,46 @@ def repository_test():
             return self.load_json_resource("github-repo.json")
 
     return RepositoryTest()
+
+
+class MockGitHubProvider:
+    def __init__(self):
+        self.rest_api = pretend.stub(
+            org=pretend.stub(),
+            repo=pretend.stub(),
+            team=pretend.stub(),
+            user=pretend.stub(),
+            app=pretend.stub(),
+        )
+        self.graphql = pretend.stub()
+        self.web = pretend.stub()
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, *args):
+        return None
+
+    #
+    #  Additional setup methods for other API calls can be added here
+    #
+    def setup_org_advisories(self, advisories_by_state):
+        async def mock_get_security_advisories(github_id, state):
+            return advisories_by_state.get(state, [])
+
+        self.rest_api.org.get_security_advisories = mock_get_security_advisories
+        return self
+
+
+@pytest.fixture()
+def mock_github_provider():
+    """Fixture that provides a configurable mock GitHub provider.
+
+    We can set up various mock responses  for different API methods.
+
+    Usage examples:
+        # Basic usage with advisories
+        mock_provider = mock_github_provider()
+        mock_provider.setup_org_advisories({"published": [advisory1, advisory2]})
+    """
+    return MockGitHubProvider
