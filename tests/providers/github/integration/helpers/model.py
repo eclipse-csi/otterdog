0204e26bd01a05ca1c39d34504fa859ce6f1108b
Alexander Lanin <alex@lanin.de>
2026-01-14 21:14:23 +0100

    add copyright header

--- tests/providers/github/integration/helpers/model.py
+++ tests/providers/github/integration/helpers/model.py
@@ -1,3 +1,11 @@
+#  *******************************************************************************
+#  Copyright (c) 2026 Eclipse Foundation and others.
+#  This program and the accompanying materials are made available
+#  under the terms of the Eclipse Public License 2.0
+#  which is available at http://www.eclipse.org/legal/epl-v20.html
+#  SPDX-License-Identifier: EPL-2.0
+#  *******************************************************************************
+
 from otterdog.models import LivePatch, LivePatchContext, ModelObject
 from otterdog.models.environment import Environment
 from otterdog.models.organization_secret import OrganizationSecret
--- tests/providers/github/integration/test_org_secrets.py
+++ tests/providers/github/integration/test_org_secrets.py
@@ -1,3 +1,11 @@
+#  *******************************************************************************
+#  Copyright (c) 2026 Eclipse Foundation and others.
+#  This program and the accompanying materials are made available
+#  under the terms of the Eclipse Public License 2.0
+#  which is available at http://www.eclipse.org/legal/epl-v20.html
+#  SPDX-License-Identifier: EPL-2.0
+#  *******************************************************************************
+
 import pytest
 
 from otterdog.models.organization_secret import OrganizationSecret
